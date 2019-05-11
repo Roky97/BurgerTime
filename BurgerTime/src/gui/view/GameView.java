2e6cccd21a17755b1b04707e094ae141c62ba36a
Roky97 <roccopalermiti@virgilio.it>
2019-05-11 20:45:55 +0200

    Now the game load Player's lifes

--- BurgerTime/src/gui/view/GameView.java
+++ BurgerTime/src/gui/view/GameView.java
@@ -1,5 +1,6 @@
 package gui.view;
 
+import java.awt.Image;
 import java.util.ArrayList;
 
 import javafx.scene.image.ImageView;
@@ -15,19 +16,25 @@ public class GameView extends ViewManager implements IView {
 	final int imageSizeX = 35;
 	final int imageSizeY = 30;
 	
+	private ArrayList<ImageView> lifesImage;
+	
 	private ArrayList<Map> levels;
 	private int totLevels = 1;
 	private int currLevel = 1;
+	private int lifes = 4;
 	
-	private String tile= "/gui/resources/game/tile.png";
-	private String stair = "/gui/resources/game/stair.png";
-	private String tileStair = "/gui/resources/game/tileStair.png";
+	
+	private String tileUrl= "/gui/resources/game/tile.png";
+	private String stairUrl = "/gui/resources/game/stair.png";
+	private String tileStairUrl = "/gui/resources/game/tileStair.png";
+	private String lifeUrl = "/gui/resources/game/life.png";
 	
 	public GameView() {
 		super(735,1000);
 //		mainStage.setResizable(false);
 		
 		levels = new ArrayList<Map>();
+		lifesImage = new ArrayList<ImageView>();
 		
 		loadLevels();
 		createBackground();
@@ -41,10 +48,26 @@ public class GameView extends ViewManager implements IView {
 			levels.add(new Map(i+1));
 //			System.out.println(levels.get(i));
 		}
+		drawLifes();
 	}
 	
+	
+	
+	private void drawLifes() {
+		for(int i=0; i<lifes; i++) {
+			ImageView lifeImage = new ImageView(lifeUrl);
+			lifeImage.setX(i*35);
+			lifeImage.setY(0);
+			lifesImage.add(lifeImage);
+			
+			pane.getChildren().add(lifeImage);
+//			pane.getChildren().remove(lifesImage.get(lifesImage.size()-1));
+		}
+		
+	}
+
 	private void drawTile(int i, int j) {
-		ImageView tileImage = new ImageView(tile);
+		ImageView tileImage = new ImageView(tileUrl);
 		tileImage.setX(j*35 );
 		tileImage.setY((i*30) + OFFSET_Y );
 		
@@ -52,16 +75,17 @@ public class GameView extends ViewManager implements IView {
 	}
 	
 	private void drawStair(int i, int j) {
-		ImageView stairImage = new ImageView(stair);
-	System.out.println(stairImage.getX());
+		ImageView stairImage = new ImageView(stairUrl);
+//	System.out.println(stairImage.getFitHeight());
 		stairImage.setX(j*35 );
 		stairImage.setY((i*30) + OFFSET_Y );
 		
 		pane.getChildren().add(stairImage);
 	}
 	
+	
 	private void drawTileStair(int i, int j) {
-		ImageView tileStairImage = new ImageView(tileStair);
+		ImageView tileStairImage = new ImageView(tileStairUrl);
 		tileStairImage.setX(j*35 );
 		tileStairImage.setY((i*30) + OFFSET_Y );
 		
